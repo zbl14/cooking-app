f163473f25d23a1df12f9012da6b8b006ff85d78
zbl14 <ifthereisoneday@gmail.com>
2022-06-02 10:16:23 -0700

    add cuptoml function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Business Logic
 function cToF(celsius) {
   return celsius * 9 / 5 + 32;
 }
@@ -5,9 +6,25 @@ function cToF(celsius) {
 function fToC(fahrenheit) {
   return (fahrenheit - 32) * 5 / 9;
 }
+function cupToMil(cups) {
+  return cups * 236.588236;
+}
+function milToCup(milliliter) {
+  return milliliter / 236.588236;
+}
+//User Interface Logic
+$(document).ready(function() {
+  $("form#cupsToMil").submit(function(event) {
+  const cups = parseInt($("#cups").val());
+  const milliliter = parseInt($("#milliter").val());
+  const result = cupToMil(cups);
+  //const milliliters = parseFloat($("#milliliter"));
+  $("#outputCup").text(result);
+  //$("#outputMil").text(milliliters);
+  // const celsius = parseInt(prompt("What is the temperature in celsius?"))
+//const fahrenheit = parseInt(prompt("What is the temperature in fahrenheit?"));
+  //alert(cToF(celsius));
+  //alert(fToC(fahrenheit));
+  });
+});
 
-const celsius = parseInt(prompt("What is the temperature in celsius?"))
-const fahrenheit = parseInt(prompt("What is the temperature in fahrenheit?"));
-
-alert(cToF(celsius));
-alert(fToC(fahrenheit));
